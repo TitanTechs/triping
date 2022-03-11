3224590b55ebc04ecec96d320fe48198d3e984a6
Verite Makuza Mugabo <mugaboverite@gmail.com>
2022-03-11 09:01:11 +0200

    fix: added fetch mode

--- src/index.ts
+++ src/index.ts
@@ -3,38 +3,37 @@ import "dotenv/config";
 import { request } from "undici";
 
 class Schedule {
-  cronJob: CronJob;
+    cronJob: CronJob;
 
-  constructor() {
-    this.cronJob = new CronJob("* * * * *", async () => {
-      try {
-        await this.pingApis();
-        await this.pingItsSelf();
-      } catch (e) {
-        console.error(e);
-      }
-    });
+    constructor() {
+        this.cronJob = new CronJob("* * * * *", async () => {
+            try {
+                await this.pingApis();
+                await this.pingItsSelf();
+            } catch (e) {
+                console.error(e);
+            }
+        });
 
-    if (!this.cronJob.running) {
-      this.cronJob.start();
+        if (!this.cronJob.running) {
+            this.cronJob.start();
+        }
     }
-  }
 
-  async pingApis() {
-    const { statusCode } = await request(process.env.HEROKU_API);
-
-    if (statusCode !== 500) {
-      console.log({ statusCode });
+    async pingApis() {
+        const { statusCode } = await request(process.env.HEROKU_API);
+        if (statusCode !== 500) {
+            console.log({ statusCode });
+        }
     }
-  }
 
-  async pingItsSelf() {
-    const { statusCode } = await request(process.env.SELF_API);
+    async pingItsSelf() {
+        const { statusCode } = await request(process.env.SELF_API);
 
-    if (statusCode !== 500) {
-      console.log({ statusCode });
+        if (statusCode !== 500) {
+            console.log({ statusCode });
+        }
     }
-  }
 }
 
 const schedule = new Schedule();
