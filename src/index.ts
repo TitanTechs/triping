8dc0bd920ceeb90ac6794a9d62e0b68219784260
Verite Makuza Mugabo <mugaboverite@gmail.com>
2022-03-11 09:14:44 +0200

    fix: update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { CronJob } from "cron";
 import "dotenv/config";
+import http from "http";
 import { request } from "undici";
 
 class Schedule {
@@ -38,4 +39,16 @@ class Schedule {
 
 const schedule = new Schedule();
 
+const server = http.createServer(function (req, res) {
+    if (req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello, World!\n');
+    } else {
+        res.writeHead(405, { 'Content-Type': 'text/plain' });
+        res.end('Method Not Allowed\n');
+    }
+})
+
+
+server.listen(process.env.PORT || 3000)
 schedule.cronJob.start();
